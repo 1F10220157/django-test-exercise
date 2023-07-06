9626c358d37f1153e4e629443b3b17e3c1b06119
1F10220157 <s1f102201571@iniad.org>
2023-07-06 14:25:39 +0900

    詳細ページのテストを修正2

--- todo/tests.py
+++ todo/tests.py
@@ -86,7 +86,7 @@ class TodoViewTestCase(TestCase):
 
     def test_detail_get_success(self):
         task = Task(title="task1", due_at=timezone.make_aware(datetime(2023, 7, 1)))
-        task1.save()
+        task.save()
         client = Client()
         response = client.get('/{}/'.format(task.pk))
 
